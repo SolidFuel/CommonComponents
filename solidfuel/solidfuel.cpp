82a155e89615f031600b1bac8b2254cb12d2d1bc
Mark Hollomon <mhhollomon@gmail.com>
2023-11-19 16:19:28 -0500

    inital commit

---
+++ solidfuel/solidfuel.cpp
@@ -0,0 +1,4 @@
+#include "solidfuel.hpp"
+
+#include "Source/BoxComponent.cpp"
+#include "Source/ButtonGroupComponent.cpp"
---
+++ solidfuel/solidfuel.hpp
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * This file declares a JUCE module for the shared library code implemented in
+ * this directory. The block below is read by the Projucer to automatically
+ * generate project code that uses the module. For details, see the
+ * JUCE Module Format.txt file in the JUCE directory.
+
+   BEGIN_JUCE_MODULE_DECLARATION
+    ID:                 solidfuel
+    vendor:             SolidFuel
+    version:            0.1.0
+    name:               SolidFuel
+    license:            GPL/Commercial
+    description:        Common modules for SolidFuel Plugins
+    minimumCppStandard: 17
+    dependencies:       juce_gui_basics
+   END_JUCE_MODULE_DECLARATION
+*******************************************************************************/
+
+#include "Source/BoxComponent.hpp"
+#include "Source/ButtonGroupComponent.hpp"
